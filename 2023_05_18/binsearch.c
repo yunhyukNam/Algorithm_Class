df3ac9013547175dcdc9564d02f931bc116356bf
yunhyukNam <yunhyuk1223@kookmin.ac.kr>
2023-05-30 09:05:47 +0900

    Algorithm Class

--- 2023_05_18/binsearch.c
+++ 2023_05_18/binsearch.c
@@ -137,3 +137,155 @@ TREENODE* insertNode_recur(TREENODE* p, char _x) {
     return p;
 }
 
+void deleteNode(TREENODE** root, element key) {
+    TREENODE *parent = NULL, *p = NULL, *succ = NULL, *succ_parent = NULL;
+    TREENODE *child = NULL;
+
+    p = *root;
+
+    while ((p != NULL) && (p->key != key)) {
+        parent = p;
+
+        if (key < p->key) {
+            p = p->left;
+        }
+        else {
+            p = p->right;
+        }
+    }
+
+    if (p == NULL) {
+        printf("\n Your Key is Not Found!\n");
+    }
+
+    if ((p->left == NULL) && (p->right == NULL)) {
+        if (parent != NULL) {
+            if (parent->left == p) {
+                parent->left = NULL;
+            }
+            else {
+                parent->right = NULL;
+            }
+        }
+        else {
+            *root = NULL;
+        }
+    }
+    else if ((p->left == NULL) || (p->right == NULL)) {
+        if (p->right != NULL) {
+            child = p->left;
+        }
+        else {
+            child = p->right;
+        }
+
+        if (parent != NULL) {
+            if (parent->left == p) {
+                parent->left = child;
+            }
+            else {
+                parent->right = child;
+            }
+        }
+    }
+    else {
+        succ_parent = p;
+
+        succ = p->left;
+
+        while (succ->right != NULL) {
+            succ_parent = succ;
+            succ = succ->right;
+        }
+
+        if (succ_parent->left == succ) {
+            succ_parent->left = succ->left;
+        }
+        else {
+            succ_parent->right = succ->right;
+        }        
+        p->key = succ->key;
+        p = succ;
+    }
+    free(p);
+}
+
+TREENODE* deleteNode_recur (TREENODE* root, int key) {
+    TREENODE* temp;
+
+    if (root == NULL) {
+        return root;
+    }
+
+    if (key < root -> key) {
+        root->left = deleteNode_recur(root->left, key);
+    }
+    else if (key > root->key) {
+        root->right = deleteNode_recur(root->right, key);
+    }
+    else {
+        if (root->left == NULL) {
+            temp = root->right;
+            free(root);
+
+            return temp;
+        }
+        else if (root->right == NULL) {
+            temp = root->left;
+
+            return temp;
+        }
+
+        temp = min_value_node(root->right);
+
+        root->key = temp->key;
+        root->right = deleteNode_recur(root->right, temp->key);
+    }
+    return root;
+}
+
+int main(void) {
+    TREENODE* root = NULL;
+    TREENODE* foundedNode = NULL;
+
+    char choice, key;
+    int cnt_i;
+
+    root = insertNode(root, 'h');
+    root = insertNode(root, 'h');
+    root = insertNode(root, 'h');
+    root = insertNode(root, 'h');
+    root = insertNode(root, 'h');
+    root = insertNode(root, 'h');
+    root = insertNode(root, 'h');
+    root = insertNode(root, 'h');
+
+    while (1) {
+        menu();
+        scanf(" %c", &choice);
+
+        switch (choice - '0') {
+        case 1:
+            displayInorder(root), printf("\n");
+            showTree(root);
+
+            break;
+        
+        case 2:
+            insertNode(&root, key);
+            insertNode_recur(root, key);
+
+            break;
+
+        case 3:
+            scanf(" %c, &key");
+            deleteNode(&root, key);
+
+        case 4:
+            scanf(" %c", &key);
+
+        default:
+            break;
+        }
+    }
+}
